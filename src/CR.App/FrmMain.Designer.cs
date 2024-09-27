49a79da50910f54435d03aa50b230aede065a826
跃瑞 段 <duanyuerui@hotmail.com>
2024-09-27 16:25:51 +0800

    添加项目文件。

---
+++ src/CR.App/FrmMain.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace CR.App
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel3 = new Panel();
+            panel5 = new Panel();
+            txtLog = new TextBox();
+            panel4 = new Panel();
+            txtRepetitionContext = new TextBox();
+            panel6 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            panel2 = new Panel();
+            btnClearLog = new Button();
+            btnGenerate = new Button();
+            btnCompare = new Button();
+            btnUpload = new Button();
+            btnChoose = new Button();
+            txtFilePath = new TextBox();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            panel5.SuspendLayout();
+            panel4.SuspendLayout();
+            panel6.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(961, 550);
+            panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(panel5);
+            panel3.Controls.Add(panel4);
+            panel3.Controls.Add(panel6);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(0, 71);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(961, 479);
+            panel3.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(txtLog);
+            panel5.Dock = DockStyle.Fill;
+            panel5.Location = new Point(494, 30);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(467, 449);
+            panel5.TabIndex = 5;
+            // 
+            // txtLog
+            // 
+            txtLog.Dock = DockStyle.Fill;
+            txtLog.ForeColor = SystemColors.ActiveBorder;
+            txtLog.Location = new Point(0, 0);
+            txtLog.Multiline = true;
+            txtLog.Name = "txtLog";
+            txtLog.Size = new Size(467, 449);
+            txtLog.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(txtRepetitionContext);
+            panel4.Dock = DockStyle.Left;
+            panel4.Location = new Point(0, 30);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(494, 449);
+            panel4.TabIndex = 4;
+            // 
+            // txtRepetitionContext
+            // 
+            txtRepetitionContext.Dock = DockStyle.Fill;
+            txtRepetitionContext.Location = new Point(0, 0);
+            txtRepetitionContext.Multiline = true;
+            txtRepetitionContext.Name = "txtRepetitionContext";
+            txtRepetitionContext.Size = new Size(494, 449);
+            txtRepetitionContext.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(label2);
+            panel6.Controls.Add(label1);
+            panel6.Dock = DockStyle.Top;
+            panel6.Location = new Point(0, 0);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(961, 30);
+            panel6.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.YellowGreen;
+            label2.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 134);
+            label2.Location = new Point(697, 4);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 22);
+            label2.TabIndex = 1;
+            label2.Text = "行为日志";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.LightCoral;
+            label1.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 134);
+            label1.Location = new Point(165, 4);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 22);
+            label1.TabIndex = 0;
+            label1.Text = "比较结果";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(btnClearLog);
+            panel2.Controls.Add(btnGenerate);
+            panel2.Controls.Add(btnCompare);
+            panel2.Controls.Add(btnUpload);
+            panel2.Controls.Add(btnChoose);
+            panel2.Controls.Add(txtFilePath);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(961, 71);
+            panel2.TabIndex = 0;
+            // 
+            // btnClearLog
+            // 
+            btnClearLog.Location = new Point(874, 36);
+            btnClearLog.Name = "btnClearLog";
+            btnClearLog.Size = new Size(75, 23);
+            btnClearLog.TabIndex = 6;
+            btnClearLog.Text = "清空日志";
+            btnClearLog.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Location = new Point(793, 36);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(75, 23);
+            btnGenerate.TabIndex = 5;
+            btnGenerate.Text = "模板数据";
+            btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // btnCompare
+            // 
+            btnCompare.Location = new Point(183, 36);
+            btnCompare.Name = "btnCompare";
+            btnCompare.Size = new Size(75, 23);
+            btnCompare.TabIndex = 4;
+            btnCompare.Text = "③ 比较";
+            btnCompare.UseVisualStyleBackColor = true;
+            // 
+            // btnUpload
+            // 
+            btnUpload.Location = new Point(102, 36);
+            btnUpload.Name = "btnUpload";
+            btnUpload.Size = new Size(75, 23);
+            btnUpload.TabIndex = 3;
+            btnUpload.Text = "② 上传";
+            btnUpload.UseVisualStyleBackColor = true;
+            // 
+            // btnChoose
+            // 
+            btnChoose.Location = new Point(12, 36);
+            btnChoose.Name = "btnChoose";
+            btnChoose.Size = new Size(84, 23);
+            btnChoose.TabIndex = 2;
+            btnChoose.Text = "① 选择文件";
+            btnChoose.UseVisualStyleBackColor = true;
+            // 
+            // txtFilePath
+            // 
+            txtFilePath.Location = new Point(12, 6);
+            txtFilePath.Name = "txtFilePath";
+            txtFilePath.Size = new Size(937, 23);
+            txtFilePath.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(961, 550);
+            Controls.Add(panel1);
+            Name = "FrmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FrmMain";
+            Load += FrmMain_Load;
+            panel1.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel3;
+        private TextBox txtRepetitionContext;
+        private Panel panel2;
+        private Button btnCompare;
+        private Button btnUpload;
+        private Button btnChoose;
+        private TextBox txtFilePath;
+        private Button btnGenerate;
+        private Panel panel5;
+        private TextBox txtLog;
+        private Panel panel4;
+        private Panel panel6;
+        private Label label2;
+        private Label label1;
+        private Button btnClearLog;
+    }
+}
---
+++ src/CR.App/FrmMain.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CR.App
+{
+    public partial class FrmMain : Form
+    {
+
+        private List<string> _dataSource = new();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            InitEvent();
+            InitControl();
+            InitControlProperty();
+            AddLog("程序初始化完成");
+        }
+
+        public void InitEvent()
+        {
+            btnChoose.Click += BtnChoose_Click;
+            btnUpload.Click += BtnUpload_Click;
+            btnCompare.Click += BtnCompare_Click;
+            btnGenerate.Click += BtnGenerate_Click;
+            btnClearLog.Click += BtnClearLog_Click;
+        }
+
+        /// <summary>
+        /// 清空行为日志
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void BtnClearLog_Click(object? sender, EventArgs e)
+        {
+            this.txtLog.Clear();
+        }
+
+        public void InitControl()
+        {
+            this.txtFilePath.PlaceholderText = "请选择数据源文件(.txt)";
+            this.txtRepetitionContext.PlaceholderText = "无重复数据";
+            this.txtLog.PlaceholderText = "操作日志 ...";
+        }
+
+        public void InitControlProperty()
+        {
+            this.txtLog.ReadOnly = true;
+        }
+
+        /// <summary>
+        ///  选择文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void BtnChoose_Click(object? sender, EventArgs e)
+        {
+            AddLog("正在选择数据源文件");
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择数据源文件";
+            ofd.Filter = "文本文件|*.txt";
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFilePath.Text = ofd.FileName;
+                AddLog("完成选择数据源文件");
+            }
+            else
+            {
+                AddLog("取消选择数据源文件");
+            }
+        }
+
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private async void BtnUpload_Click(object? sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFilePath.Text.Trim()))
+            {
+                MessageBox.Show("请选择数据源文件!!!", "提示信息");
+                return;
+            }
+
+            string fileName = GetFileName(this.txtFilePath.Text.Trim());
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                MessageBox.Show("请选择正确的数据源文件路径!!!", "提示信息");
+                return;
+            }
+
+            AddLog($"正在上传文件【{fileName}】");
+            _dataSource.Clear(); // 清空所有数据源
+            string context = await File.ReadAllTextAsync(this.txtFilePath.Text.Trim());
+            _dataSource.AddRange(context.Split("\r\n", StringSplitOptions.RemoveEmptyEntries));
+            AddLog("文件上传完成");
+        }
+
+        /// <summary>
+        /// 比较数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void BtnCompare_Click(object? sender, EventArgs e)
+        {
+            if (!_dataSource.Any())
+            {
+                MessageBox.Show("请上传数据!!!", "提示信息");
+                return;
+            }
+            txtRepetitionContext.Clear(); // 比较前清除所有内容
+            AddLog($"正在比较数【{GetFileName(this.txtFilePath.Text.Trim())}】的据源");
+            //比较逻辑
+            CompareDataSource();
+
+
+            AddLog("数据源比较完毕");
+        }
+
+        /// <summary>
+        /// 生成默认模板数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private async void BtnGenerate_Click(object? sender, EventArgs e)
+        {
+            AddLog("正在下载模板数据");
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("agjslgkngsg");
+            sb.AppendLine("128jfgaskl5");
+            sb.AppendLine("12519025jad");
+            sb.AppendLine("sdjkgsadg92");
+            sb.AppendLine("15298jkdfa9");
+            sb.AppendLine("128jfgaskl5");
+
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "请选择要保存的路径";
+            sfd.FileName = "样例数据模板.txt";
+            sfd.Filter = "文本文件|*.txt";
+            sfd.RestoreDirectory = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = sfd.FileName;
+                await File.WriteAllTextAsync(filePath, sb.ToString());
+            }
+
+            AddLog("模板数据下载完成");
+        }
+
+
+        public void AddLog(string log)
+        {
+            this.txtLog.Invoke(() =>
+            {
+                this.txtLog.AppendText($"{log} ...");
+                this.txtLog.AppendText("\r\n");
+            });
+        }
+
+        public void CompareDataSource()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            _dataSource.ForEach(x =>
+            {
+
+                if (dict.ContainsKey(x))
+                {
+                    this.txtRepetitionContext.AppendText($"数据【{x}】重复了 \r\n");
+                    dict[x]++;
+                }
+                else
+                {
+                    dict.Add(x, 1);
+                }
+            });
+
+            this.txtRepetitionContext.AppendText($"总计: \r\n");
+
+            var sumUpData = dict.Where(x => x.Value > 1);
+            if (sumUpData.Any())
+            {
+                foreach (var x in sumUpData)
+                {
+                    this.txtRepetitionContext.AppendText($"数据【{x.Key}】重复了【{x.Value - 1}】次 \r\n");
+                }
+            }
+            else
+            {
+                this.txtRepetitionContext.AppendText($"数据源无重复数据");
+            }
+        }
+
+
+        public string GetFileName(string filePath)
+        {
+            bool isFilePath = Path.HasExtension(filePath) && !Path.GetInvalidPathChars().Intersect(filePath).Any();
+            if (!isFilePath) return string.Empty;
+            return Path.GetFileName(filePath);
+        }
+    }
+}
